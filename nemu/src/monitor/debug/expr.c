2168e13c2fa2910b50268843e6e7b5795e1437a8
杨清宇 <775434852@qq.com>
2017-03-12 02:01:23 +0800

    PA1.2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -35,7 +35,7 @@ static struct rule {
 	{"\\)", ')'},                   //right bracket
 
 
-	{" && ", AND},					//And 
+	{"&&", AND},					//And 
 	{"\\|\\|", OR},					//OR
 	{"!", NO},						//NOT
 	{"%", '%'},						//complementation
@@ -107,7 +107,7 @@ static bool make_token(char *e) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
-				//Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 
 //ADD Part
